ad86196f1efc373b2cca013fd8e4c51d9b365110
Hullaah <umaradelo1.247@gmail.com>
2024-07-20 13:22:58 +0100

    edited first task solution

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Creates a users table if it does not exist
 CREATE TABLE IF NOT EXISTS users (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
 );
